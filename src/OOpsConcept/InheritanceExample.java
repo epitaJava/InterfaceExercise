452c4f2951acb419726c10851b2cb20adbc14885
Vaishnavi <147934171+epitaJava@users.noreply.github.com>
2023-10-21 00:33:03 +0200

    Add files via upload

---
+++ src/OOpsConcept/InheritanceExample.java
@@ -0,0 +1,19 @@
+package oppsConcept;
+
+class InheritanceExample {
+		
+	public void inherit() {
+		System.out.println("Hello");
+	}
+	public void inheritClass1(){
+		System.out.println("Hello World");
+	}
+}
+
+class InheritanceExample1 extends InheritanceExample{
+	
+	
+	public void inherit2() {
+		System.out.println("Java!!");
+	}
+}
---
+++ src/OOpsConcept/Main.java
@@ -0,0 +1,17 @@
+package oppsConcept;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Parentclass object = new ChileClass();
+		InheritanceExample1 in = new InheritanceExample1();
+		in.inherit();
+		in.inheritClass1();
+		
+		Bank b = new Bank();
+		b.customerPin();
+		
+	}
+
+}
